8bf9775852f9c66973859ac18ddc5289ebc75805
SHRUSTI CHOUGHULE <shrustichoughule@gmail.com>
2023-12-22 22:31:36 +0530

    new changes

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyx: "fff" },
-//   "HELLO WORLD REACT"
-// );
-{
-  /* <div id="parent">
-        <div id="child">
-            <h1>I am h1 tag</h1>
-            <h2>I am h2 tag</h2> //if we want to add h2 in react than we need to create array. 
-        </div>
-</div> */
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
- [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ]),
-      React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-      ])
- ]
-);
-
-console.log(parent); //object
+const jsxHeading = <h1 className="heading">Namaskar React using JSX</h1>;
+console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading); 
+
+
+
